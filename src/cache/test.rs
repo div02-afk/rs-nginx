8510c76cc7eaeadf0e1f77abd8e70bdca9cb79ff
Divyam Sharma <113417008+div02-afk@users.noreply.github.com>
2025-10-20 15:00:47 +0530

    Refactor LRU cache implementation to use Arc and RwLock for thread safety and async support

--- src/cache/test.rs
+++ src/cache/test.rs
@@ -1,20 +1,22 @@
 #[cfg(test)]
 mod tests {
+    use test_log::test;
     use std::{ path::{ Path, PathBuf }, sync::Arc, thread::spawn };
-
     use crate::cache::lru::Cache;
 
     use super::*; // import symbols from parent module
-    use tokio::{ self, test };
-    #[test]
+    use tokio::{ self, task::JoinHandle };
+
+    #[test(tokio::test)]
     async fn test_single_threaded_lru_cache() {
-        let mut cache = Cache::new(1024);
+        let cache = Cache::new(1024);
         let path = "/";
         let key = Path::new(path).to_path_buf();
         let data = b"Hello, world!".to_vec();
         cache.add(&key, &data).await;
 
         println!("Added to cache");
+
         let result = cache.get(&key).await;
 
         if result.is_some() {
@@ -24,5 +26,33 @@ mod tests {
         }
     }
 
+    #[test(tokio::test)]
+    async fn test_multithreaded_lru_cache() {
+        let cache = Arc::new(Cache::new(1024));
+        let paths = vec!["/", "/a", "/aa"];
+        let mut handles = vec![];
+        for path in paths {
+            let cloned_cache = cache.clone();
 
+            let handle = tokio::spawn(async move {
+                let key = Path::new(path).to_path_buf();
+                let data = b"Hello, world!".to_vec();
+                cloned_cache.add(&key, &data).await;
+
+                println!("Added {:?} to cloned_cache", key.clone());
+                let result = cloned_cache.get(&key).await;
+
+                if result.is_some() {
+                    println!("Getting from cache: {:?}", Some(result));
+                } else {
+                    println!("Failed to get results");
+                }
+            });
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.await.unwrap();
+        }
+    }
 }
--- src/cache/util.rs
+++ src/cache/util.rs
@@ -1,35 +1,37 @@
-use std::{ cell::RefCell, rc::{ Rc, Weak } };
+use std::{ sync::{ Arc, Weak } };
+
+use tokio::sync::RwLock;
 
 use crate::cache::lru::{ Cache, CacheList };
 
-pub fn add_after_head(head: &Rc<RefCell<CacheList>>, node: &Rc<RefCell<CacheList>>) {
-    let mut head = head.borrow_mut();
-    let mut mut_node = node.borrow_mut();
+pub async  fn add_after_head(head: &Arc<RwLock<CacheList>>, node: &Arc<RwLock<CacheList>>) {
+    let mut head = head.write().await;
+    let mut mut_node = node.write().await;
     mut_node.next = None;
 
     //plxacing current node's prev to old first
     if let Some(old_first) = head.next.take() {
-        mut_node.prev = Some(Rc::downgrade(&old_first));
+        mut_node.prev = Some(Arc::downgrade(&old_first));
         drop(mut_node);
         //placing head's next to current node
         head.next = Some(node.clone());
         drop(head);
 
         //placing old first node's next to current node
-        old_first.borrow_mut().next = Some(node.to_owned());
+        old_first.write().await.next = Some(node.to_owned());
         drop(old_first);
     } else {
         head.next = Some(node.clone());
     }
 }
 
-pub fn move_node_to_head(
-    head: &Rc<RefCell<CacheList>>,
-    node: &Weak<RefCell<CacheList>>,
+pub async  fn move_node_to_head(
+    head: &Arc<RwLock<CacheList>>,
+    node: &Weak<RwLock<CacheList>>,
     data: Option<&Vec<u8>>
 ) {
     if let Some(current_node) = node.upgrade() {
-        let mut current_mut_node = current_node.borrow_mut(); //got the current node
+        let mut current_mut_node = current_node.write().await; //got the current node
 
         //update old data
         if let Some(data) = data {
@@ -40,22 +42,24 @@ pub fn move_node_to_head(
         if let Some(prev_weak) = &current_mut_node.prev {
             // getting prev node's weak ref
             if let Some(prev_rc) = prev_weak.upgrade() {
-                let mut prev_node = prev_rc.borrow_mut();
+                let mut prev_node = prev_rc.write().await;
                 // now `prev_node` is a mutable ref to the actual previous node
                 prev_node.next = current_mut_node.next.clone();
+                drop(prev_node);
                 if let Some(next_rc) = &current_mut_node.next {
-                    let mut next_node = next_rc.borrow_mut();
+                    let mut next_node = next_rc.write().await;
                     // now `next_node` is a mutable ref to the actual next node
                     next_node.prev = Some(prev_weak.clone());
+                    drop(next_node);
                 }
             }
         }
         drop(current_mut_node);
 
         //placing current node to head
-        add_after_head(head, &current_node)
+        add_after_head(head, &current_node).await
     }
 }
 
 //TODO : implement last node purge
-pub fn purge(cache: &mut Cache) {}
+pub fn purge(cache: &Cache) {}
